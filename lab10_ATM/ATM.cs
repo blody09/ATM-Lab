86500f7c54443ec988e184c8a2d35f43d5367ddc
blody09 <blakeody9@gmail.com>
2020-02-04 08:13:47 -0500

    first upload of ATM

---
+++ lab10_ATM/ATM.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace lab10_ATM
+{
+    class ATM
+    {
+        private Account CurrentAccount { get; set; }
+        private static List<Account> accounts = new List<Account>();
+
+        //Methods for all atm actions
+        //login?
+        //Withdraw?
+        //Deposite
+        //end session
+    }
+}
---
+++ lab10_ATM/Account.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace lab10_ATM
+{
+    class Account
+    {
+        public string Name 
+        { get;
+          set;
+        }
+        public string Password
+        { get;
+          set;
+        }
+
+        public double Balance
+        {
+            get;
+            set;
+        }
+
+    }
+}
